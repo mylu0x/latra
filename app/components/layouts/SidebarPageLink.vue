f548defbaae2486c596f1e05dcd0a2667705588d
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-26 21:13:06 +0900

    style(sidebar): adjust design

--- app/components/layouts/SidebarPageLink.vue
+++ app/components/layouts/SidebarPageLink.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 
 <template>
   <NuxtLink :to class="flex items-center gap-8px p-5px w-130px rounded-12px hover:bg-gray-1 transition-all-100">
-    <span class="flex items-center justify-center p-5px bg-white b-(solid 1px gray-2) rounded-8px shadow-sm">
+    <span class="flex items-center justify-center p-6px bg-white b-(solid 1px gray-2) rounded-8px shadow-sm">
       <Icon :name="icon" class="text-gray-8" />
     </span>
     <span class="leading-none font-500 text-14px">{{ name }}</span>
