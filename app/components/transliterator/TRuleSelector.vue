003205770d3ea257b6aa3733b4b7493369b0109b
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-27 17:40:05 +0900

    style(ruleselector): adjust vertical align of description

--- app/components/transliterator/TRuleSelector.vue
+++ app/components/transliterator/TRuleSelector.vue
@@ -23,11 +23,11 @@ const isOpen = ref<boolean>(false);
     <div v-show="isOpen" class="absolute mt-32px left-1/2 translate-x-[-50%]">
       <div class="translate-x-[1/2] bg-white z-999 b-(solid 1px gray-2) w-400px shadow-md p-6px rounded-14px">
         <button v-for="rule in lang.rules" @click="model = rule.id" class="flex justify-between p-10px hover:bg-gray-1 rounded-8px">
-        <span class="flex flex-col justify-center w-100px text-left">
-          <span class="font-600 text-gray-9 text-18px">{{ rule.name }}</span>
-          <span class="text-10px text-gray-7 font-mono font-500">{{ rule.id }}</span>
-        </span>
-          <span class="text-12px text-gray-7 text-left">{{ rule.desc }}</span>
+          <span class="flex flex-col justify-center w-100px text-left">
+            <span class="font-600 text-gray-9 text-18px">{{ rule.name }}</span>
+            <span class="text-10px text-gray-7 font-mono font-500">{{ rule.id }}</span>
+          </span>
+          <span class="text-12px text-gray-7 text-left flex items-center">{{ rule.desc }}</span>
         </button>
       </div>
     </div>
