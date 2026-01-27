ef6b5d9060e972934ff268e138eb90b96809eb28
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-27 18:46:30 +0900

    feat(home): remake updates section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 type Update = {
-  date: string;
+  date: Date;
   content: string;
 }
 
 const updates: Update[] = [
   {
-    date: '2026-01-27',
+    date: new Date('2026-01-27'),
     content: 'Added the GOST 7.79 rule for Russian'
   },
   {
-    date: '2026-01-27',
+    date: new Date('2026-01-27'),
     content: 'Added Russian transliterator'
   }
 ];
@@ -24,9 +24,9 @@ const updates: Update[] = [
     <section class="text-center text-gray-7">
       <p><strong class="font-600 text-gray-8">Latra</strong> is a web app that converts characters into Latin script.</p>
     </section>
-    <section class="max-w-400px mx-auto mx-8px b-(solid 1px gray-2) px-24px py-14px rounded-16px shadow-sm mt-32px h-200px overflow-y-auto">
-      <div v-for="update in updates" class="flex gap-8px justify-between">
-        <p class="text-gray-8 font-600">{{ update.date }}</p>
+    <section class="mx-auto mx-8px px-24px py-20px b-t-(solid 1px gray-2) mt-48px h-200px overflow-y-auto flex flex-col gap-16px">
+      <div v-for="update in updates" class="flex flex-col justify-between text-14px">
+        <p class="text-gray-8 font-600">{{ update.date.toLocaleDateString('en-US', { dateStyle: 'long' }) }}</p>
         <p class="text-gray-8">{{ update.content }}</p>
       </div>
     </section>
