ceb511d60a7af007b391bf7b62c584b9d94f95e5
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-27 12:59:03 +0900

    feat(home): add updates section (still no update)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+type Update = {
+  date: string;
+  content: string;
+}
 
+const updates: Update[] = [
+    {
+      date: '2026-01-27',
+      content: 'a'
+    }
+];
 </script>
 
 <template>
@@ -10,6 +20,12 @@
     <section class="text-center text-gray-7">
       <p><strong class="font-600 text-gray-8">Latra</strong> is a web app that converts characters into Latin script.</p>
     </section>
+<!--    <section class="max-w-400px mx-auto mx-8px b-(solid 1px gray-2) px-24px py-14px rounded-16px shadow-sm mt-32px">-->
+<!--      <div v-for="update in updates" class="flex gap-8px">-->
+<!--        <p class="text-gray-8 font-600">{{ update.date }}</p>-->
+<!--        <p class="text-gray-8">{{ update.content }}</p>-->
+<!--      </div>-->
+<!--    </section>-->
   </div>
 </template>
 
