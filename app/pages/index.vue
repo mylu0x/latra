377835fb622700e2dd6804d0c620069bd345f565
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-27 17:41:43 +0900

    feat(home): add updates section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,10 +5,10 @@ type Update = {
 }
 
 const updates: Update[] = [
-    {
-      date: '2026-01-27',
-      content: 'a'
-    }
+  {
+    date: '2026-01-27',
+    content: 'Added Russian transliterator'
+  }
 ];
 </script>
 
@@ -20,12 +20,12 @@ const updates: Update[] = [
     <section class="text-center text-gray-7">
       <p><strong class="font-600 text-gray-8">Latra</strong> is a web app that converts characters into Latin script.</p>
     </section>
-<!--    <section class="max-w-400px mx-auto mx-8px b-(solid 1px gray-2) px-24px py-14px rounded-16px shadow-sm mt-32px">-->
-<!--      <div v-for="update in updates" class="flex gap-8px">-->
-<!--        <p class="text-gray-8 font-600">{{ update.date }}</p>-->
-<!--        <p class="text-gray-8">{{ update.content }}</p>-->
-<!--      </div>-->
-<!--    </section>-->
+    <section class="max-w-400px mx-auto mx-8px b-(solid 1px gray-2) px-24px py-14px rounded-16px shadow-sm mt-32px h-200px overflow-y-auto">
+      <div v-for="update in updates" class="flex gap-8px justify-between">
+        <p class="text-gray-8 font-600">{{ update.date }}</p>
+        <p class="text-gray-8">{{ update.content }}</p>
+      </div>
+    </section>
   </div>
 </template>
 
