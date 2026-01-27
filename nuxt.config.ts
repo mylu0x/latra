fe079e35ecfaf7c06f930b7cbb46fadcdbd5a94c
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-27 16:03:45 +0900

    chore(deps): add VueUse and its Nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/hints', '@nuxt/icon', '@nuxt/image', '@nuxt/fonts', '@unocss/nuxt'],
+  modules: ['@nuxt/hints', '@nuxt/icon', '@nuxt/image', '@nuxt/fonts', '@unocss/nuxt', '@vueuse/nuxt'],
   app: {
     head: {
       title: 'Latra',
