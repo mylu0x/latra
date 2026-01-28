087df5413e14fd76394aed4d53c835451e58d6ca
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-28 18:50:30 +0900

    chore(transliterator): set readonly and spellchecker false

--- app/components/transliterator/TResult.vue
+++ app/components/transliterator/TResult.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 
 <template>
   <div class="flex flex-col">
-    <textarea :value class="resize-none b-(solid 1px gray-2) px-8px py-4px rounded-8px bg-gray-50 outline-(solid 0 gray-1) transition-all-50 focus:outline-2px h-200px"></textarea>
+    <textarea :value readonly spellcheck="false" class="resize-none b-(solid 1px gray-2) px-8px py-4px rounded-8px bg-gray-50 outline-(solid 0 gray-1) transition-all-50 focus:outline-2px h-200px"></textarea>
   </div>
 </template>
 
