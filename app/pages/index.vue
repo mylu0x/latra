c9a3748672b2e96203e1dd9f998dec97b7fae2f0
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-29 16:18:45 +0900

    docs(home): add an update log for the latest update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,10 @@ type Update = {
 
 const updates: Update[] = [
   {
+    date: new Date('2026-01-29'),
+    content: 'Added Mylstyle A and B rules to Ukrainian'
+  },
+  {
     date: new Date('2026-01-28'),
     content: 'Added Ukrainian transliterator'
   },
@@ -36,7 +40,7 @@ const updates: Update[] = [
     <section class="text-center text-gray-7">
       <p><strong class="font-600 text-gray-8">Latra</strong> is a web app that converts characters into Latin script.</p>
     </section>
-    <section class="mx-auto mx-8px px-24px py-20px b-t-(solid 1px gray-2) mt-48px h-full overflow-y-auto flex flex-col gap-16px">
+    <section class="mx-auto mx-8px px-24px py-20px b-t-(solid 1px gray-2) mt-48px h-300px overflow-y-auto flex flex-col gap-16px">
       <div v-for="update in updates" class="flex flex-col justify-between text-14px">
         <p class="text-gray-8 font-600">{{ update.date.toLocaleDateString('en-US', { dateStyle: 'long' }) }}</p>
         <p class="text-gray-8">{{ update.content }}</p>
