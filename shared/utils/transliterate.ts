4625860d43ea48e1aa8135bf627a42e33f3d7c8a
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-29 20:23:10 +0900

    fix: transliterate logic

--- shared/utils/transliterate.ts
+++ shared/utils/transliterate.ts
@@ -1,8 +1,6 @@
 import { transliterationMap } from '#shared/data/transliterationMap';
 
 export default function (writing: string, lang: string, rule: string, input: string): string {
-  let text = input;
-
   const selectedWriting = transliterationMap[writing];
   if (!selectedWriting) throw new Error(`Writing ${writing} is not found`);
 
@@ -12,20 +10,22 @@ export default function (writing: string, lang: string, rule: string, input: str
   const selectedRule = selectedLang.rules[rule];
   if (!selectedRule) throw new Error(`Rule ${rule} is not found`);
 
-  const normalizedInput = input.normalize('NFC');
+  let normalizedInput = input.normalize('NFC');
 
   // for Greek
   if (selectedLang.langCode === 'el') {
-    text = text
+    normalizedInput = normalizedInput
       .replace(/ΑΥ/g, 'AU').replace(/Αυ/g, 'Au').replace(/αΥ/g, 'aU').replace(/αυ/g, 'au')
       .replace(/ΑΎ/g, 'AÚ').replace(/Αύ/g, 'Aú').replace(/αΎ/g, 'aÚ').replace(/αύ/g, 'aú')
       .replace(/ΕΥ/g, 'EU').replace(/Ευ/g, 'Eu').replace(/εΥ/g, 'eU').replace(/ευ/g, 'eu')
       .replace(/ΕΎ/g, 'EÚ').replace(/Εύ/g, 'Eú').replace(/εΎ/g, 'eÚ').replace(/εύ/g, 'eú')
       .replace(/ΟΥ/g, 'OU').replace(/Ου/g, 'Ou').replace(/οΥ/g, 'oU').replace(/ου/g, 'ou')
       .replace(/ΟΎ/g, 'OÚ').replace(/Ού/g, 'Oú').replace(/οΎ/g, 'oÚ').replace(/ού/g, 'oú')
+  } else {
+    console.log('its not el')
   }
 
-  text = Array.from(normalizedInput).map(char => selectedRule.map[char] ?? char).join('')
+  normalizedInput = Array.from(normalizedInput).map(char => selectedRule.map[char] ?? char).join('')
 
-  return text;
+  return normalizedInput;
 }
