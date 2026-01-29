cc269ee30a162e626eaf5a40eaf3530dab65b4ca
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-29 16:01:06 +0900

    style: adjust background color of action bar

--- app/components/transliterator/TActionBar.vue
+++ app/components/transliterator/TActionBar.vue
@@ -10,7 +10,7 @@ const getClasses = (direction: 'top' | 'bottom') => {
 
 <template>
   <div class="flex gap-6px px-8px justify-between max-md:flex-col max-md:gap-0">
-    <div class="bg-gray-1 flex p-4px gap-4px max-md:mb-0 max-md:justify-between" :class="getClasses('top')">
+    <div class="bg-gray-2 flex p-4px gap-4px max-md:mb-0 max-md:justify-between" :class="getClasses('top')">
       <button @click="$emit('copyUserInput')" class="text-14px flex items-center justify-center w-80px py-6px hover:bg-gray-1 transition-all-50 bg-white rounded-8px b-(solid 1px gray-2) shadow-sm gap-3px text-gray-9 leading-none max-md:w-full">
         <Icon name="tabler:copy" />
         <span>Copy</span>
@@ -21,7 +21,7 @@ const getClasses = (direction: 'top' | 'bottom') => {
       </button>
     </div>
     <slot />
-    <div class="bg-gray-1 flex p-4px gap-4px max-md:mt-0 max-md:justify-between" :class="getClasses('bottom')">
+    <div class="bg-gray-2 flex p-4px gap-4px max-md:mt-0 max-md:justify-between" :class="getClasses('bottom')">
       <button @click="$emit('copyResult')" class="text-14px flex items-center justify-center w-80px py-6px hover:bg-gray-1 transition-all-50 bg-white rounded-8px b-(solid 1px gray-2) shadow-sm gap-3px text-gray-9 leading-none max-md:w-full">
         <Icon name="tabler:copy" />
         <span>Copy</span>
