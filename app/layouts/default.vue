872501bef49149ef1d5eed18d2f46d4e16beb4ad
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-01-29 19:50:02 +0900

    fix(design): layout slot was behind GitHub link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="flex flex-col items-center justify-center w-screen h-screen px-16px">
-    <div class="h-500px max-w-800px w-full mx-auto b-(solid 1px gray-2) p-8px rounded-18px shadow-sm flex gap-8px bg-white max-md:flex-col max-md:h-auto">
+    <div class="h-500px max-w-800px w-full mx-auto b-(solid 1px gray-2) p-8px rounded-18px shadow-sm flex gap-8px bg-white max-md:flex-col max-md:h-auto max-md:mt-30px">
       <TheSidebar />
       <div class="flex h-full w-1px bg-gray-1 max-md:w-full max-md:h-1px"></div>
       <div class="w-full">
