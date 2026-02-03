930a02898ffc0315430832ddf64392b5de7f91ea
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-02-03 18:59:32 +0900

    fix(layout): top margin

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center w-screen h-screen px-16px">
-    <div class="h-500px max-w-800px w-full mx-auto b-(solid 1px gray-2) p-8px rounded-18px shadow-sm flex gap-8px bg-white max-md:flex-col max-md:h-auto max-md:mt-30px">
+  <div class="flex flex-col items-center justify-center w-screen h-screen px-16px max-md:mt-150px">
+    <div class="h-500px max-w-800px w-full mx-auto b-(solid 1px gray-2) p-8px rounded-18px shadow-sm flex gap-8px bg-white max-md:flex-col max-md:h-auto">
       <TheSidebar />
       <div class="flex h-full w-1px bg-gray-1 max-md:w-full max-md:h-1px"></div>
       <div class="w-full">
